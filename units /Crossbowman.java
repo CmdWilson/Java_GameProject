4f44c6a1884570fc3ee35652eb9872f8c9b5e7c6
CmdWilson <141258229+CmdWilson@users.noreply.github.com>
2024-01-18 17:52:27 +0300

    Create Crossbowman.java

---
+++ units /Crossbowman.java
@@ -0,0 +1,23 @@
+package units;
+
+import java.util.ArrayList;
+
+public class Crossbowman extends Archer {
+    public Crossbowman(String name, int x, int y) {
+        super(name, 150, 150, 10,
+                30, 1, 0.2, 0.3, x, y, true);
+        arrows = 8;
+        attackDistance = 6;
+    }
+
+    @Override
+    public String toString() {
+        return ("Crossbowman: " + super.toString());
+    }
+
+    @Override
+    public void step(ArrayList<HeroBase> enemies,ArrayList<HeroBase> allies) {
+        super.step(enemies,allies);
+        super.step(enemies,allies);
+    }
+}
