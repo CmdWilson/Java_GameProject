6b0fd46d1c465a46eaf9342a59fabf11c5f4fd5d
CmdWilson <141258229+CmdWilson@users.noreply.github.com>
2024-01-18 17:52:07 +0300

    Create Coordinates.java

---
+++ units /Coordinates.java
@@ -0,0 +1,24 @@
+package units;
+
+public class Coordinates {
+    public int x,y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public float distance(Coordinates enemyCoord){
+        return (float) Math.sqrt(Math.pow(x - enemyCoord.x,2) + Math.pow(y - enemyCoord.y,2));
+    }
+
+    public Coordinates deltaCoordinates(HeroBase enemy){
+        Coordinates enemyCoordinates = enemy.position;
+        return new Coordinates(this.x - enemyCoordinates.x,this.y - enemyCoordinates.y );
+    }
+}
